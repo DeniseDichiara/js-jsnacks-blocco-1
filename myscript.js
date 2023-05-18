0aebca8ccd8c75eff92ca9d13b14e4bdb778ba90
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-18 17:25:38 +0200

    sum

--- myscript.js
+++ myscript.js
@@ -1,7 +1,9 @@
 
 
-for(i = 0; i < 10; i ++){
+for(i = 0; i < 3; i ++){
     let userNumber = parseInt(prompt('Enter a number '));
-    const somma = userNumber
-    console.log(somma);
+    
+    let sum = parseInt(userNumber * i)
+
+    console.log (sum)
 }
